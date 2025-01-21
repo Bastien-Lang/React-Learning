55e2ea9f1591a6d639f955e8c9359f2c84127d39
Loup Olivier <loup.olivier@gmail.com>
2025-01-21 12:41:09 +0100

    Modification de la page App.jsx pour personnalisation et ajout d'un fichier Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {myTask} from "./Data/data.js";
 import './App.css'
 
 function App() {
     let name = 'Toto'
   return (
-      <div className="App">
-          <p>
-              Hello {name}!
-          </p>
-      </div>
+      <>
+          <div className="App">
+              <p>
+                  Hello {name}!
+              </p>
+          </div>
+          <div className='Data'>
+                <h1>My Tasks</h1>
+                <ul>
+                    {myTask.map((task) => (
+                        <li key={task.id}>{task.name}</li>
+                    ))}
+                </ul>
+          </div>
+      </>
   )
 }
 
---
+++ src/Data/data.js
@@ -0,0 +1,16 @@
+let myTask = [
+    {
+        id: 1,
+        name: 'Learn React',
+    },
+    {
+        id: 2,
+        name: 'Learn Vite',
+    },
+    {
+        id: 3,
+        name: 'Learn CSS',
+    }
+]
+
+export {myTask}
